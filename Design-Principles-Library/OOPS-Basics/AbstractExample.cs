c7f93a9824877b2c64df15715e2cd9f1b1ccadc7
Pradip Bobhate <pradip.bobhate@live.in>
2017-04-12 23:38:05 -0700

    Oops basics

---
+++ Design-Principles-Library/OOPS-Basics/AbstractExample.cs
@@ -0,0 +1,51 @@
+﻿namespace Design_Principles_Library.OOPS_Basics
+{
+    internal class AbstractExample
+    {
+        // compile with: /target:library
+        public class D
+        {
+            public virtual void DoWork(int i)
+            {
+                // Original implementation.
+            }
+
+            //It is an error to use the abstract modifier on a static property.
+        }
+
+        /// <summary>
+        /// An abstract class defines the core identity of a class and there it is used for objects of the same type.
+        /// </summary>
+
+        public abstract class E : D
+        {
+            //An abstract class can contain access modifiers for the subs, functions, properties
+            //Overriden from base class D and marked as abstract
+            //so that it will be defined in derived class of E
+            public abstract override void DoWork(int i);
+
+            /// <summary>
+            /// An abstract method is implicitly a virtual method.
+            /// </summary>
+            /// <param name="i"></param>
+            protected abstract void DoWork_Protected(int i);
+
+            //It is an error to use the static or virtual modifiers in an abstract method declaration.
+        }
+
+        public class F : E
+        {
+            //An abstract inherited property can be overridden in a derived class
+            //by including a property declaration that uses the override modifier.
+            public override void DoWork(int i)
+            {
+                // New implementation.
+            }
+
+            protected override void DoWork_Protected(int i)
+            {
+                // New implementation.
+            }
+        }
+    }
+}
---
+++ Design-Principles-Library/OOPS-Basics/InterfaceExample.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Design_Principles_Library.OOPS_Basics
+{
+    /// <summary>
+    /// Interfaces are used to define the peripheral abilities of a class.
+    /// </summary>
+
+    //An interface cannot have access modifiers for the subs, functions, properties etc
+    //everything is assumed as public
+    internal class InterfaceExample1
+    {
+        private interface ISampleInterface
+        {
+            void SampleMethod();
+        }
+
+        private class ImplementationClass : ISampleInterface
+        {
+            // Explicit interface member implementation:
+            void ISampleInterface.SampleMethod()
+            {
+                // Method implementation.
+            }
+
+            private static void Main()
+            {
+                // Declare an interface instance.
+                ISampleInterface obj = new ImplementationClass();
+
+                // Call the member.
+                obj.SampleMethod();
+            }
+        }
+    }
+
+    internal class InterfaceExample2
+    {
+        //Interface contains the property declaration and the class contains the implementation.
+
+        private interface IPoint
+        {
+            // Property signatures:
+            int x
+            {
+                get;
+                set;
+            }
+
+            int y
+            {
+                get;
+                set;
+            }
+        }
+
+        private class Point : IPoint
+        {
+            // Fields:
+            private int _x;
+
+            private int _y;
+
+            // Constructor:
+            public Point(int x, int y)
+            {
+                _x = x;
+                _y = y;
+            }
+
+            // Property implementation:
+            public int x
+            {
+                get
+                {
+                    return _x;
+                }
+
+                set
+                {
+                    _x = value;
+                }
+            }
+
+            public int y
+            {
+                get
+                {
+                    return _y;
+                }
+                set
+                {
+                    _y = value;
+                }
+            }
+        }
+
+        private class MainClass
+        {
+            private static void PrintPoint(IPoint p)
+            {
+                Console.WriteLine("x={0}, y={1}", p.x, p.y);
+            }
+
+            private static void Main()
+            {
+                Point p = new Point(2, 3);
+                Console.Write("My Point: ");
+                PrintPoint(p);
+            }
+        }
+
+        // Output: My Point: x=2, y=3
+    }
+}
