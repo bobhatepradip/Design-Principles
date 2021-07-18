fb3f6950be94567f990ac4586f9f33032c84261e
Pradip Bobhate <bobhate.pradip@gmail.com>
2021-07-18 09:45:30 +0530

    date

--- Design-Principles-Library/Solids/03_Liskov_Substitution_Principle.cs
+++ Design-Principles-Library/Solids/03_Liskov_Substitution_Principle.cs
@@ -2,85 +2,97 @@
 
 namespace Design_Principles_Library
 {
-    internal class _03_Liskov_Substitution_Principle
+    // If any module is using a Base class then the reference to that Base class can be replaced with a Derived class without affecting the functionality of the module.
+
+    // Or
+
+    // While implementing derived classes, one needs to ensure that, derived classes just extend the functionality of base classes without replacing the functionality of base classes.
+    public class Rectangle
     {
-        // If any module is using a Base class then the reference to that Base class can be replaced with a Derived class without affecting the functionality of the module.
+        protected int mWidth = 0;
+        protected int mHeight = 0;
 
-        // Or
-
-        // While implementing derived classes, one needs to ensure that, derived classes just extend the functionality of base classes without replacing the functionality of base classes.
-        private class Rectangle
+        public virtual void SetWidth(int width)
         {
-            protected int mWidth = 0;
-            protected int mHeight = 0;
+            Console.WriteLine(string.Format("Rectangle Width:{0}", width));
+            mWidth = width;
+        }
 
-            public virtual void SetWidth(int width)
+        public virtual void SetHeight(int height)
+        {
+            Console.WriteLine(string.Format("Rectangle Width:{0}", height));
+            mHeight = height;
+        }
+
+        public virtual int GetArea()
+        {
+            Console.WriteLine(string.Format("Rectangle Area:"));
+            return mWidth * mHeight;
+        }
+    }
+
+    // While implementing derived class if one replaces the functionality of base class then,
+    // it might results into undesired side effects when such derived classes are used in existing program modules.
+    public class Square : Rectangle
+    {
+        // This class modifies the base class functionality instead of extending the base class functionality
+
+        // Now below methods implementation will impact base class functionality.
+        public override void SetWidth(int width)
+        {
+            Console.WriteLine(string.Format("Square Width and Height:{0}", width));
+            mWidth = width;
+            mHeight = width;
+        }
+
+        public override void SetHeight(int height)
+        {
+            Console.WriteLine(string.Format("Square Height/Width:{0}", height));
+            mWidth = height;
+            mHeight = height;
+        }
+
+        public override int GetArea()
+        {
+            Console.WriteLine(string.Format("$$$ Square Area:"));
+            if (mWidth<10)
             {
-                mWidth = width;
+                throw new Exception("width should be less than 10");
             }
+            return mWidth * mHeight;
+        }
+    }
 
-            public virtual void SetHeight(int height)
+    public static class LiskovSubstitutionPrincipleDemo
+    {
+        public static Rectangle CreateInstance(bool isRectangle)
+        {
+            // As per Liskov Substitution Principle "Derived types must be completely substitutable for their base types".
+            //bool isRectangle = false;
+            if (isRectangle == true)
             {
-                mHeight = height;
+                return new Rectangle();
             }
-
-            public virtual int GetArea()
+            else
             {
-                return mWidth * mHeight;
+                return new Square();
             }
         }
 
-        // While implementing derived class if one replaces the functionality of base class then,
-        // it might results into undesired side effects when such derived classes are used in existing program modules.
-        private class Square : Rectangle
+        public static void LSPDemo(bool isRectangle)
         {
-            // This class modifies the base class functionality instead of extending the base class functionality
+            Console.WriteLine("\n\nLiskov Substitution Principle Demo ");
 
-            // Now below methods implementation will impact base class functionality.
-            public override void SetWidth(int width)
-            {
-                mWidth = width;
-                mHeight = width;
-            }
+            Rectangle RectangleObject = CreateInstance(isRectangle);
 
-            public override void SetHeight(int height)
-            {
-                mWidth = height;
-                mHeight = height;
-            }
-        }
+            // User assumes that RectangleObject is a rectangle and (s)he is able to set the width and height as for the base class
+            RectangleObject.SetWidth(5);
+            RectangleObject.SetHeight(10);
 
-        private class LiskovSubstitutionPrincipleDemo
-        {
-            private static Rectangle CreateInstance()
-            {
-                // As per Liskov Substitution Principle "Derived types must be completely substitutable for their base types".
-                bool SomeCondition = false;
-                if (SomeCondition == true)
-                {
-                    return new Rectangle();
-                }
-                else
-                {
-                    return new Square();
-                }
-            }
+            // Now this results into the area 100 (10 * 10 ) instead of 50 (10 * 5).
+            Console.WriteLine("Liskov Substitution Principle has been violated and returned wrong result : " + RectangleObject.GetArea());
 
-            public static void LSPDemo()
-            {
-                Console.WriteLine("\n\nLiskov Substitution Principle Demo ");
-
-                Rectangle RectangleObject = CreateInstance();
-
-                // User assumes that RectangleObject is a rectangle and (s)he is able to set the width and height as for the base class
-                RectangleObject.SetWidth(5);
-                RectangleObject.SetHeight(10);
-
-                // Now this results into the area 100 (10 * 10 ) instead of 50 (10 * 5).
-                Console.WriteLine("Liskov Substitution Principle has been violated and returned wrong result : " + RectangleObject.GetArea());
-
-                // So once again I repaet that sub classes should extend the functionality, sub classes functionality should not impact base class functionality.
-            }
+            // So once again I repaet that sub classes should extend the functionality, sub classes functionality should not impact base class functionality.
         }
     }
 }
--- Design-Principles-Runner/Program.cs
+++ Design-Principles-Runner/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Design_Principles
+﻿using System;
+using Design_Principles_Library;
+
+namespace Design_Principles
 {
-    internal class Program
+    public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
+            Console.WriteLine("\n\n Start : Liskov Substitution Principle Demo ");
+            Console.WriteLine("\n\n==========Rectangle=========== ");
+            bool isRectangle = true;
+            LiskovSubstitutionPrincipleDemo.LSPDemo(isRectangle);
+            //Console.ReadKey();
+            Console.WriteLine("\n\n==========Square=========== ");
+            isRectangle = false;
+            LiskovSubstitutionPrincipleDemo.LSPDemo(isRectangle);
+            Console.WriteLine("\n\nEnd : Liskov Substitution Principle Demo ");
+            Console.ReadKey();
         }
     }
 }
